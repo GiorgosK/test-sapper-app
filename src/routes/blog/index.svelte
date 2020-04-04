c51cf58fef0faf209a1c25d110f2e259a5b2c843
Giorgos Kontopoulos <kongeo@gmail.com>
2020-04-04 11:12:17 +0300

    Add class identifier on blog-posts list

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -23,7 +23,7 @@
 
 <h1>Recent posts</h1>
 
-<ul>
+<ul class='blog-posts'>
 	{#each posts as post}
 		<!-- we're using the non-standard `rel=prefetch` attribute to
 				tell Sapper to load the data for the page as soon as
